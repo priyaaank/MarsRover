962ac3212ee1c5c4181791fb1915944c4760331b
Chirag Doshi <chirag.doshi@gmail.com>
2014-07-02 08:17:12 +0530

    Update DirectionTest.java

--- test/universe/DirectionTest.java
+++ test/universe/DirectionTest.java
@@ -8,6 +8,11 @@ public class DirectionTest {
 
     @Test
     public void testThatThereIsAlwaysSingleInstanceForNorthDirection() {
+        // Chirag: you can inline the northDirectionOne and northDirectionTwo variables
+        // Chirag: now this is a bit debatable but in such easy to understand tests, 
+        // I would lose the descriptive names of testcases and simply go with test1(), test2()
+        // I am too lazy to type such long names when the test code is so  easy to read than the name itself
+        // or maybe in this case it makes sense to break the one assert per test rule and plunk all these assertSame()s in one testcase
         //Given
         Direction northDirectionOne = Direction.north();
         Direction northDirectionTwo = Direction.north();
